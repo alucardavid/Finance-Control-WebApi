80969c4106084f67284ef7cd1e56630c2a3fd1c3
David Pereira <d_pereira@outlook.com.br>
2022-06-04 21:31:46 -0300

    Add project files.

---
+++ Endpoints/Balances/BalanceGetAll.cs
@@ -0,0 +1,16 @@
+﻿namespace Control_Finance_WebAPI.Endpoints.Balances
+{
+    public class BalanceGetAll
+    {
+        public static string Template => "/balances";
+        public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+        public static Delegate Handle => Action;
+
+        public static IResult Action()
+        {
+            
+
+            return Results.Ok("Ok");
+        }
+    }
+}
